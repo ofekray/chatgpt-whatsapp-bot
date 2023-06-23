0affdff90f87f738b9ea0a495722b9ae12e96179
Ofek Bashan <ofekray@gmail.com>
2023-06-23 14:28:43 +0000

    ZZZ

--- deployment/ffmpeg-installer.sh
+++ deployment/ffmpeg-installer.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-mkdir layer
-cd layer
-curl -O https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
-tar xf ffmpeg-git-amd64-static.tar.xz
-rm ffmpeg-git-amd64-static.tar.xz
-mv ffmpeg-git-*-amd64-static ffmpeg
-cd ..
+if [ ! -d "layer" ] 
+then
+    mkdir layer
+    cd layer
+    curl -O https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+    tar xf ffmpeg-git-amd64-static.tar.xz
+    rm ffmpeg-git-amd64-static.tar.xz
+    mv ffmpeg-git-*-amd64-static ffmpeg
+    cd ..
+fi
